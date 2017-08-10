015c9b1973b1515ca9f311e46a3a58d8ca0e1606
shenjiaqi <shenjiaqi@baidu.com>
2017-08-10 13:53:37 +0800

    add eventbus

---
+++ eventbus/app/src/main/java/com/example/liuwangshu/mooneventbus/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.liuwangshu.mooneventbus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv_message;
+    private Button bt_message;
+    private Button bt_subscription;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt_subscription=(Button)this.findViewById(R.id.bt_subscription);
+        bt_subscription.setText("订阅事件");
+        bt_message=(Button)this.findViewById(R.id.bt_message);
+        bt_message.setText("跳转到SecondActivity");
+        bt_message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              startActivity(new Intent(MainActivity.this,SecondActivity.class));
+            }
+        });
+        bt_subscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //注册事件
+                if(!EventBus.getDefault().isRegistered(MainActivity.this)) {
+                    EventBus.getDefault().register(MainActivity.this);
+                }else{
+                    Toast.makeText(MainActivity.this,"请勿重复注册事件",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //取消注册事件
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMoonEvent(MessageEvent messageEvent){
+        tv_message.setText(messageEvent.getMessage());
+    }
+    @Subscribe(sticky = true)
+    public void ononMoonStickyEvent(MessageEvent messageEvent){
+        tv_message.setText(messageEvent.getMessage());
+    }
+}
