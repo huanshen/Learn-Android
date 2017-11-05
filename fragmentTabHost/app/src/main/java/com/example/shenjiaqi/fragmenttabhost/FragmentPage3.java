4dc8f44370f8eedecf8807f6abb9e081887a89c5
shenjiaqi <shenjiaqi@baidu.com>
2017-11-05 22:27:50 +0800

    add new work

---
+++ fragmentTabHost/app/src/main/java/com/example/shenjiaqi/fragmenttabhost/FragmentPage3.java
@@ -0,0 +1,21 @@
+package com.example.shenjiaqi.fragmenttabhost;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by shenjiaqi on 2017/11/5.
+ */
+
+public class FragmentPage3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_3, null);
+    }
+}
