3a5180fe76518a7b521d15b7c5d82eb1245000e2
shenjiaqi <shenjiaqi@baidu.com>
2017-11-19 23:08:20 +0800

    redtore back

--- TablayoutViewpager/app/src/main/java/com/crazyfzw/tablayoutviewpager/MainActivity.java
+++ TablayoutViewpager/app/src/main/java/com/crazyfzw/tablayoutviewpager/MainActivity.java
@@ -25,15 +25,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
