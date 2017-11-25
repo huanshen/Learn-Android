4d912c16c22b8c5eb4a667b9657fd4daaaeac07b
shenjiaqi <shenjiaqi@baidu.com>
2017-11-26 00:08:11 +0800

    add popupWindow

---
+++ PopupWindow/app/src/main/java/com/example/shenjiaqi/popupwindow/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.shenjiaqi.popupwindow;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private PopupWindow mPopWindow;
+    private Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupWindow();
+            }
+        });
+    }
+
+    private void showPopupWindow() {
+        //设置contentView
+        View contentView = LayoutInflater.from(MainActivity.this).inflate(R.layout.popup_window, null);
+        mPopWindow = new PopupWindow(contentView,
+                ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT, true);
+        mPopWindow.setContentView(contentView);
+        //设置各个控件的点击响应
+        TextView tv1 = (TextView)contentView.findViewById(R.id.pop_computer);
+        TextView tv2 = (TextView)contentView.findViewById(R.id.pop_financial);
+        TextView tv3 = (TextView)contentView.findViewById(R.id.pop_manage);
+        tv1.setOnClickListener(this);
+        tv2.setOnClickListener(this);
+        tv3.setOnClickListener(this);
+        //显示PopupWindow
+        View rootview = LayoutInflater.from(MainActivity.this).inflate(R.layout.activity_main, null);
+        mPopWindow.setAnimationStyle(R.style.contextMenuAnim);
+        //Log.i("shenjiaqi",)
+        //mPopWindow.showAtLocation(rootview, Gravity.TOP|Gravity.LEFT, 100, 456);
+
+        mPopWindow.showAsDropDown(button, -300, -300);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.pop_computer:{
+                Toast.makeText(this,"clicked computer", Toast.LENGTH_SHORT).show();
+                mPopWindow.dismiss();
+            }
+            break;
+            case R.id.pop_financial:{
+                Toast.makeText(this,"clicked financial",Toast.LENGTH_SHORT).show();
+                mPopWindow.dismiss();
+            }
+            break;
+            case R.id.pop_manage:{
+                Toast.makeText(this,"clicked manage",Toast.LENGTH_SHORT).show();
+                mPopWindow.dismiss();
+            }
+            break;
+        }
+    }
+}
+
